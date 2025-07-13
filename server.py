428d3c49e65681d1c193155bccac49710e503c7a
pavankumarcs <cspkd5508@gmail.com>
2025-07-13 07:36:00 -0400

    Update Flask server.py to return JSON for emotion detection

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/emotionDetector')
+def emotion_detector_route():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    result = {
+        "anger": response["anger"],
+        "disgust": response["disgust"],
+        "fear": response["fear"],
+        "joy": response["joy"],
+        "sadness": response["sadness"],
+        "dominant_emotion": response["dominant_emotion"]
+    }
+
+    return jsonify(result)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
